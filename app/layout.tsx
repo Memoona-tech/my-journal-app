e3ac68bc2dd9709a8e02882ca974c628abd08a23
Memoona Saleem <memoonas2024@gmail.com>
2025-06-04 06:26:16 +0500

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,12 @@ export const metadata = {
   title: 'MyJournal 🌷',
   description: 'Write your thoughts beautifully.',
   icons: {
-    icon: "/favicon.png", // or "/favicon.ico"
+    icon: [
+      {
+        url: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌷</text></svg>',
+        rel: 'icon',
+      },
+    ],
   },
 }
 
